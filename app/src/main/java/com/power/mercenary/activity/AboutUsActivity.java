ae5c471e38b7b7d9a39eb8974123ddd299312995
fuxingjfl <1119832643@qq.com>
2018-03-23 11:55:28 +0800

    负心

--- app/src/main/java/com/power/mercenary/activity/AboutUsActivity.java
+++ app/src/main/java/com/power/mercenary/activity/AboutUsActivity.java
@@ -27,7 +27,6 @@ public class AboutUsActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_about_us);
         ButterKnife.bind(this);
 
