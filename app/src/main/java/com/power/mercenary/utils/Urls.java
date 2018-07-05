a50c9ad18b6cf93bebf37284e4afdd666f572e4c
wl <wangle1109@outlook.com>
2018-07-05 09:47:16 +0800

    w

--- app/src/main/java/com/power/mercenary/utils/Urls.java
+++ app/src/main/java/com/power/mercenary/utils/Urls.java
@@ -2,5 +2,5 @@ package com.power.mercenary.utils;
 
 public class Urls {
     public static String FILENAME = "config.xml";
-    public static String BASEURL = "";
+    public static String BASEURL = "http://yb.dashuibei.com/index.php/";
 }
