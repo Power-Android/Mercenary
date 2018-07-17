21c06c9e8990debf258243bccdbf242022cc0c2e
wl <wangle1109@outlook.com>
2018-07-17 19:01:50 +0800

    w

--- app/src/main/java/com/power/mercenary/activity/SetupActivity.java
+++ app/src/main/java/com/power/mercenary/activity/SetupActivity.java
@@ -384,21 +384,22 @@ public class SetupActivity extends BaseActivity implements View.OnClickListener,
                 .imageSpanCount(4)// 每行显示个数
                 .selectionMode(PictureConfig.SINGLE)// 多选 or 单选 PictureConfig.SINGLE
                 .previewImage(true)// 是否可预览图片
-                .previewVideo(true)// 是否可预览视频
-                .enablePreviewAudio(true) // 是否可播放音频
-                .compressGrade(Luban.THIRD_GEAR)// luban压缩档次，默认3档 Luban.FIRST_GEAR、Luban.CUSTOM_GEAR
+//                .previewVideo(true)// 是否可预览视频
+//                .enablePreviewAudio(true) // 是否可播放音频
+//                .compressGrade(Luban.THIRD_GEAR)// luban压缩档次，默认3档 Luban.FIRST_GEAR、Luban.CUSTOM_GEAR
                 .isCamera(true)// 是否显示拍照按钮
                 .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
                 //.setOutputCameraPath("/CustomPath")// 自定义拍照保存路径
                 .enableCrop(true)// 是否裁剪
                 .compress(true)// 是否压缩
+                .withAspectRatio(4, 4)
                 .compressMode(PictureConfig.LUBAN_COMPRESS_MODE)//系统自带 or 鲁班压缩 PictureConfig.SYSTEM_COMPRESS_MODE or LUBAN_COMPRESS_MODE
                 //.sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
-                .glideOverride(200, 200)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+//                .glideOverride(200, 200)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
                 //                .withAspectRatio(aspect_ratio_x, aspect_ratio_y)// 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
                 .hideBottomControls(true)// 是否显示uCrop工具栏，默认不显示
                 .isGif(false)// 是否显示gif图片
-                .freeStyleCropEnabled(true)// 裁剪框是否可拖拽
+                .freeStyleCropEnabled(false)// 裁剪框是否可拖拽
                 .circleDimmedLayer(true)// 是否圆形裁剪
                 .showCropFrame(false)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
                 .showCropGrid(false)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
@@ -412,7 +413,7 @@ public class SetupActivity extends BaseActivity implements View.OnClickListener,
                 //.compressWH() // 压缩宽高比 compressGrade()为Luban.CUSTOM_GEAR有效
                 //.cropWH()// 裁剪宽高比，设置如果大于图片本身宽高则无效
                 //.rotateEnabled() // 裁剪是否可旋转图片
-                .scaleEnabled(false)// 裁剪是否可放大缩小图片
+                .scaleEnabled(true)// 裁剪是否可放大缩小图片
                 //.videoQuality()// 视频录制质量 0 or 1
                 //.videoSecond()//显示多少秒以内的视频or音频也可适用
                 //.recordVideoSecond()//录制视频秒数 默认60s
@@ -424,13 +425,14 @@ public class SetupActivity extends BaseActivity implements View.OnClickListener,
                 .openCamera(PictureMimeType.ofImage())// 单独拍照，也可录像或也可音频 看你传入的类型是图片or视频
                 .theme(R.style.picture_default_style)// 主题样式设置 具体参考 values/styles
                 .enableCrop(true)// 是否裁剪
+                .withAspectRatio(4, 4)
                 .compress(true)// 是否压缩
                 .compressMode(PictureConfig.LUBAN_COMPRESS_MODE)//系统自带 or 鲁班压缩 PictureConfig.SYSTEM_COMPRESS_MODE or LUBAN_COMPRESS_MODE
-                .freeStyleCropEnabled(true)// 裁剪框是否可拖拽
+                .freeStyleCropEnabled(false)// 裁剪框是否可拖拽
                 .circleDimmedLayer(true)// 是否圆形裁剪
                 .showCropFrame(false)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
                 .showCropGrid(false)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
-                .scaleEnabled(false)// 裁剪是否可放大缩小图片
+                .scaleEnabled(true)// 裁剪是否可放大缩小图片
                 //                .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
                 //                .selectionMedia(list)// 是否传入已选图片
                 //                .previewEggs(true)//预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中)
